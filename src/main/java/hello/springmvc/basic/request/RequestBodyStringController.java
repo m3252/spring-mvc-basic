7c643a98bd7235d0f438832297484ce42e520fe8
seungchan.moon <moon.unimewo@gmail.com>
2022-07-06 12:33:17 +0900

    feat : 요청 메시지 단순 텍스트 ing

---
+++ src/main/java/hello/springmvc/basic/request/RequestBodyStringController.java
@@ -0,0 +1,20 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Slf4j
+@Controller
+public class RequestBodyStringController {
+
+    @PostMapping("/request-body-string-v1")
+    public void requestBodyString(HttpServletRequest request, HttpServletResponse response) throws IOException {
+
+    }
+
+}
