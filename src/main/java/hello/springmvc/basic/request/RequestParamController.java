2533e93ea1a42583c9e0508ef9235969f3390d72
seungchan.moon <moon.unimewo@gmail.com>
2022-07-06 12:15:56 +0900

    feat : 요청 파라미터 @ModelAttribute

--- src/main/java/hello/springmvc/basic/request/RequestParamController.java
+++ src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -1,7 +1,9 @@
 package hello.springmvc.basic.request;
 
+import hello.springmvc.basic.HelloData;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -78,5 +80,26 @@ public class RequestParamController {
         return "ok";
     }
 
+    @ResponseBody
+    @RequestMapping("/model-attribute-v1")
+    public String modelAttributeV1(
+            //@RequestParam String username, @RequestParam int age
+            @ModelAttribute HelloData helloData
+    ) {
+//        HelloData helloData = new HelloData();
+//        helloData.setUsername(username);
+//        helloData.setAge(age);
+        log.info("username ={}, age ={}", helloData.getUsername(), helloData.getAge());
+        log.info("helloData ={}", helloData);
+        return "ok";
+    }
+
+
+    @ResponseBody
+    @RequestMapping("/model-attribute-v2")
+    public String modelAttributeV2(HelloData helloData) {
+        log.info("helloData ={}", helloData);
+        return "ok";
+    }
 
 }
